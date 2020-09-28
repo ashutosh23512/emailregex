a455413b8306ff88499d3adf04040879ef75547b
ASHUTOSH <ashutosh@192.168.1.4>
2020-09-28 17:50:17 +0530

    [Ashutosh]Added:Second Part

--- src/com/emailregex/EmailRegex.java
+++ src/com/emailregex/EmailRegex.java
@@ -10,7 +10,7 @@ public class EmailRegex {
 		System.out.println("Enter Email Address");
 		Scanner s=new Scanner(System.in);
 		String email=s.nextLine();
-		Pattern pattern=Pattern.compile("^[a-z A-Z]+");		
+		Pattern pattern=Pattern.compile("^[a-z A-Z]+@{1}[a-z A-Z]+");		
 		Matcher matcher=pattern.matcher(email);
 		
 		boolean matchFound=matcher.find();
